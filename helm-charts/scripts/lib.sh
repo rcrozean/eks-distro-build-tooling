d05a15d830170a2e07899f5044537a2113c5a1f0
Cameron Rozean <rcrozean@amazon.com>
2023-02-08 14:15:39 -0800

    modify whitespace

--- helm-charts/scripts/lib.sh
+++ helm-charts/scripts/lib.sh
@@ -13,7 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-
 CHART_BUCKET="prow-data-devstack-prowchartsbucket2e50b8d9-cx2cst36nww3"
 
 REPO_URL="https://${CHART_BUCKET}.s3.amazonaws.com"
